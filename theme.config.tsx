48c081b89203a4c009e805e42d5522dc564baa5a
Elvis Saravia <ellfae@gmail.com>
2023-03-12 13:14:15 -0600

    organize pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,27 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Prompt Engineering Guide</span>,
+  logo: (
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 206 246" fill="none">
+        <circle cx="40" cy="40" r="40" fill="currentColor"/>
+        <circle cx="40" cy="206" r="40" fill="currentColor"/>
+        <circle cx="166" cy="120" r="40" fill="currentColor"/>
+      </svg>
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+        Prompt Engineering Guide
+      </span>
+    </>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/dair-ai/Prompt-Engineering-Guide',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/SKgkVT8BGJ',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/dair-ai/Prompt-Engineering-Guide/tree/main/',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Copyright © 2023 DAIR.AI',
   },
 }
 
