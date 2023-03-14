677f9a570d40ed2b38a0615c26e28ee63d0cab41
Elvis Saravia <ellfae@gmail.com>
2023-03-13 21:13:09 -0600

    add favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: (
@@ -14,6 +15,28 @@ const config: DocsThemeConfig = {
       </span>
     </>
   ),
+  head: function UseHead() {
+    const { title } = useConfig()
+    return (
+    <>
+      <title>{title ? title + ' | Prompt Engineering Guide': 'Prompt Engineering Guide'} </title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Prompt Engineering Guide" />
+      <meta property="og:description" content="A Comprehensive Overview of Prompt Engineering" />
+      <meta
+          name="og:title"
+          content={title ? title + ' | Prompt Engineering Guide' : 'Prompt Engineering Guide'}
+        />
+      <link rel="icon" href="/144-favicon.svg" type="image/svg+xml" />
+
+        <link
+          rel="icon"
+          href="/144-favicon-dark.svg"
+          type="image/svg+xml"
+          media="(prefers-color-scheme: dark)"
+        />
+    </>
+  )},
   project: {
     link: 'https://github.com/dair-ai/Prompt-Engineering-Guide',
   },
