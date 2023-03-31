b18274dd3e3928adbb614b96c151512974f42bdb
Elvis Saravia <ellfae@gmail.com>
2023-03-30 18:43:20 -0600

    multilanguage support

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  i18n: {
+    locales: ['en', 'zh'],
+    defaultLocale: 'en'
+  }
+})
