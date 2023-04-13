a3dfba0eb4276ac684d86ff21e30aefada902eba
Elvis Saravia <ellfae@gmail.com>
2023-04-13 11:03:00 -0600

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -20,7 +20,7 @@ const config: DocsThemeConfig = {
     { locale: 'zh', text: '中文' },
     { locale: 'jp', text: '日本語'},
     { locale: 'pt', text: 'Português' },
-    { locale: 'it': text: 'Italian' },
+    { locale: 'it', text: 'Italian' },
     { locale: 'tr', text: 'Türkçe' },
   ],
   head: function UseHead() {
