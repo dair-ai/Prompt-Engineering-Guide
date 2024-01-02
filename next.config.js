c8c2d2c79af0d767fb2687b5b8e654f478770918
Elvis Saravia <ellfae@gmail.com>
2024-01-02 15:28:40 -0600

    function calling notes

--- next.config.js
+++ next.config.js
@@ -9,4 +9,18 @@ module.exports = withNextra({
     locales: ['en', 'zh', 'jp', 'pt', 'tr', 'es', 'it', 'fr', 'kr', 'ca', 'fi', 'ru','de'],
     defaultLocale: 'en',
   },
+  webpack(config) {
+    const allowedSvgRegex = /components\/icons\/.+\.svg$/
+
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test?.('.svg')
+    )
+    fileLoaderRule.exclude = allowedSvgRegex
+
+    config.module.rules.push({
+      test: allowedSvgRegex,
+      use: ['@svgr/webpack']
+    })
+    return config
+  }
 })
